45be5448839d2895e1739d271b71f44e9897165f
Brian Blosser <bpb54321@gmail.com>
2019-02-16 16:13:16 -0500

    Got Behat set up and working.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,13 +1,15 @@
 <?php
 
-use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\RawMinkContext;
+use \Behat\Behat\Tester\Exception\PendingException;
+use \PHPUnit\Framework\Assert;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends Behat\MinkExtension\Context\RawMinkContext
+class FeatureContext extends RawMinkContext
 {
     /**
      * Initializes context.
@@ -18,5 +20,31 @@ class FeatureContext extends Behat\MinkExtension\Context\RawMinkContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * Visits the given url in the browser.
+     * Example: I visit "https://google.com"
+     *
+     * @Given I visit :url
+     *
+     * @param string $url The url to visit.
+     */
+    public function iVisitUrl($url)
+    {
+        $session = $this->getSession();
+        $session->visit($url);
+    }
+
+    /**
+     * Pause for a certain amount of time.
+     *
+     * @Given /^I pause for (\d+) seconds$/
+     *
+     * @param $num_seconds
+     */
+    public function iPauseForSeconds($num_seconds)
+    {
+        sleep($num_seconds);
     }
 }
---
+++ features/start-app.feature
@@ -0,0 +1,6 @@
+Feature: Start the app
+
+  Scenario: I visit the Google Assistant Simulator
+
+    Given I visit "https://www.google.com"
+    And I pause for 10 seconds
