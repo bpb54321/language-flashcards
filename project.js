21a8f375a419b509cd8b98559b2abcf233426278
Brian Blosser <bpb54321@gmail.com>
2019-07-30 11:53:28 -0400

    Updated project.js with AWS Lambda url.

--- project.js
+++ project.js
@@ -9,6 +9,14 @@ module.exports = {
     googleAction: {
        nlu: 'dialogflow',
     },
-    endpoint: '${JOVO_WEBHOOK_URL}',
+    defaultStage: 'local',
+    states: {
+      local: {
+        endpoint: '${JOVO_WEBHOOK_URL}',
+      },
+      dev: {
+        endpoint: 'arn:aws:lambda:us-east-1:043616026455:function:jovoFlashcardApp',
+      },
+    }
 };
  
