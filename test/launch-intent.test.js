c2cd206904ef329cba366127371cf7c21083cbb9
Brian Blosser <bpb54321@gmail.com>
2019-08-30 15:23:03 -0400

    Added a test testing what happens when the user says no to studying flashcards.

--- test/launch-intent.test.js
+++ test/launch-intent.test.js
@@ -12,5 +12,18 @@ describe(`Launch Intent` , () => {
         const response = await conversation.send(launchRequest);
         expect(response.getSpeech()).toBe(`welcome`);
     });
+
+    test(`should say goodbye if the user says that they don't want to study flashcards`, async () => {
+        const conversation = testSuite.conversation({ locale: `keys-only` });
+
+        const request = await testSuite.requestBuilder.intent();
+        request.addSessionData(`_JOVO_STATE_`, `StudyState`);
+        request.setIntentName(`NoIntent`);
+
+        const response = await conversation.send(request);
+
+        console.log(response);
+        expect(response.getSpeech()).toBe(`sorry_you_dont_want_to_play`);
+    });
 });
 
